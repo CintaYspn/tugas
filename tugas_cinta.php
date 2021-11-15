393966cbc1a9ed559b0496010dbf0cda5323f212
CintaYspn <90019845+CintaYspn@users.noreply.github.com>
2021-11-15 21:53:58 +0700

    Update tugas_cinta.php
    cek

--- tugas_cinta.php
+++ tugas_cinta.php
@@ -24,9 +24,9 @@
 if (isset($_POST['submit'])) {
 	$bintang= $_POST['bintang'];
 	for ($i=1; $i <= $bintang ; $i++) { 
-		for ($c=1; $c <= $i ; $c++) { 
-			echo "*";
-		}
+	for ($c=1; $c <= $i ; $c++) { 
+		echo "*";
+	}
 		echo "<br>";
 	}
 }
